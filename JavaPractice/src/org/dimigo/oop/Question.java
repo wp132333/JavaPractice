158554aba9ede6232b2f0919c30ea02df23aea23
CJM <CJM@Karon>
2015-05-12 11:55:21 +0900

    ㅁㄴㄹ

---
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+import java.util.Scanner;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_ Question
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 5. 12.
+ * </pre>
+ *
+ * @author : CJM
+ * @version : 1.0
+ * 
+ */
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("가장 좋아하는 가수는?");
+		String answer = scanner.nextLine();
+		if(answer.equals("빅뱅")) {
+			System.out.println("정답입니다.");
+		}else {
+			System.out.println("틀렸습니다.");
+		}
+		System.out.println("가장 좋아하는 배우는?");
+		answer = scanner.nextLine();
+		if(answer.equals("조인성")) {
+			System.out.println("정답입니다.");
+		}else {
+			System.out.println("틀렸습니다.");
+		}
+		System.out.println("가장 좋아하는 과목는?");
+		answer = scanner.nextLine();
+		if(answer.equals("자바")) {
+			System.out.println("정답입니다.");
+		}else {
+			System.out.println("틀렸습니다.");
+		}
+		
+		
+	}
+
+}
