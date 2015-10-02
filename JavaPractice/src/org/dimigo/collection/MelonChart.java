9e00e7c865f4f8cc2351b1336cd41936d800f94a
CJM <CJM@Karon>
2015-10-02 23:51:40 +0900

    ㅁㄴㅇ

--- JavaPractice/src/org/dimigo/collection/MelonChart.java
+++ JavaPractice/src/org/dimigo/collection/MelonChart.java
@@ -1,29 +1,48 @@
-///**
-// * 
-// */
-//package org.dimigo.collection;
-//
-//import java.util.List;
-//
-///**
-// * <pre>
-// * org.dimigo.collection
-// *   |_ MelonChart
-// *
-// * 1. 개요 : 
-// * 2. 작성일 : 2015. 9. 25.
-// * </pre>
-// *
-// * @author : CJM
-// * @version : 1.0
-// * 
-// */
-//public class MelonChart {
-//	public static void main(String[] args) {
-//		
-//	}
-//	public static void List(List list){
-//		
-//	}
-//
-//}
+/**
+ * 
+ */
+package org.dimigo.collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * <pre>
+ * org.dimigo.collection
+ *   |_ MelonChart
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 9. 25.
+ * </pre>
+ * @author : CJM
+ * @version : 1.0
+ * 
+ */
+public class MelonChart {
+	public static void main(String[] args) {
+		List<Music> list = new ArrayList<Music>();
+		list.add(new Music("바람이나 좀 쐐" , "개리"));
+		list.add(new Music("보통연애" , "박경"));
+		list.add(new Music("취향저격" , "iKON"));
+		System.out.println("-- << 멜론 챠트 >> --");
+		printList(list);
+		System.out.println("-- << 2위 곡 추가 >> --");
+		list.add(1,new Music("레옹" , "이유갓지"));
+		printList(list);
+		System.out.println("-- << 3위 곡 변경 >> --");
+		list.set(2, new Music("맙소사" , "황태지"));
+		printList(list);
+		System.out.println("-- << 전체 리스트 삭제 >> --");
+		list.clear();
+		printList(list);
+		
+	}
+	public static void printList(List<Music> list){
+		Iterator<Music> i = list.iterator(); 
+		while(i.hasNext()){
+			System.out.println(i.next());
+		}
+	}
+
+}
